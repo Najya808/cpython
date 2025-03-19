059f6440760a0b83d8538cf6e6876f6cd4504701
Najya808 <najyaqureshi333@gmail.com>
2025-03-19 11:06:33 +0500

    Update python.c
    Added #ifdef _WIN32 to conditionally use wmain for Windows platforms. Used main for non-Windows platforms. Fixed preprocessor syntax by adding the necessary.

--- Programs/python.c
+++ Programs/python.c
@@ -3,14 +3,12 @@
 #include "Python.h"
 
 #ifdef MS_WINDOWS
-int
-wmain(int argc, wchar_t **argv)
+int wmain(int argc, wchar_t **argv)
 {
     return Py_Main(argc, argv);
 }
 #else
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     return Py_BytesMain(argc, argv);
 }
